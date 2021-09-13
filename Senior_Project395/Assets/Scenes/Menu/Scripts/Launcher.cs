85a5edc802604b72a53c54191b5c40e08b072d5a
Jack Dunne <57883799+JackDunne5877@users.noreply.github.com>
2021-09-13 17:36:53 -0400

    Squashed commit of the following:
    commit da6658c277f3702e5e0ae2a8d2bb0f5bfb2a2acd
    Merge: b1a5aa8 293cc8e
    Author: Jack Dunne <57883799+JackDunne5877@users.noreply.github.com>
    Date:   Mon Sep 13 17:05:57 2021 -0400
    
        Merge branch 'main' of https://github.com/JackDunne5877/Senior-Project-395 into main
    
    commit 293cc8e27d0953a43af05fb877ff99f434ad1c64
    Merge: 870ea07 22780a5
    Author: Jack Dunne <57883799+JackDunne5877@users.noreply.github.com>
    Date:   Sun Sep 12 17:25:16 2021 -0400
    
        Merge pull request #5 from JackDunne5877/Multiplayer
    
        Multiplayer Minimum Viable Prototype
    
    commit 22780a53bbc7f8aac30e4ea354d82b2fefe73286
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Sun Sep 12 17:21:35 2021 -0400
    
        reorganized beach scene
    
    commit a4b188818dc596450a9b8d9342d9538c4395ecde
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Sun Sep 12 16:34:43 2021 -0400
    
        two players can join the same game!
    
    commit ceb98c3d237c69499f5b6a4c4a53bb1dfd866234
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Sun Sep 12 13:25:51 2021 -0400
    
        menu now loads photon servers
    
    commit 0b022f4500037bd54e24f30faf9b4a27be8aadd1
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Sun Sep 12 12:59:30 2021 -0400
    
        added to mp demo scene
    
    commit e242fe70b0921d872cb61c01c667e3b8f0d857d4
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Wed Sep 8 20:55:22 2021 -0400
    
        some network code added to multiplayer scene
    
    commit f8c2748afa601073f379ca074c36ef4ab58fa2a5
    Author: Orion Follett <orionfollett@gmail.com>
    Date:   Wed Sep 8 19:50:23 2021 -0400
    
        added multiplayer test scene

---
+++ Senior_Project395/Assets/Scenes/Menu/Scripts/Launcher.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.Orion.MP
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+
+
+        [Tooltip("The level that loads when play is clicked")]
+        [SerializeField]
+        private int levelIndex = 1;
+
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 2;
+        
+        [Tooltip("The UI Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+        
+        #endregion
+
+
+        #region Private Fields
+
+        /// <summary>
+        /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+        /// </summary>
+        string gameVersion = "1";
+        bool isConnecting;
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Start the connection process.
+        /// - If already connected, we attempt joining a random room
+        /// - if not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        public void Connect()
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.IsConnected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+        #endregion
+
+        #region MonoBehaviourPunCallbacks Callbacks
+        public override void OnConnectedToMaster()
+        {
+            if (isConnecting)
+            {
+                Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            Debug.LogWarningFormat("Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+            isConnecting = false;
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("Load level index: " + levelIndex);
+
+                // #Critical
+                // Load the Level.
+                PhotonNetwork.LoadLevel(levelIndex);
+            }
+        }
+
+        #endregion
+    }
+}
