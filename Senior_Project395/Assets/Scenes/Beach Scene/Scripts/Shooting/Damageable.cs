7d08fe4b803620da94822c1c16ee8f9fc8f8834f
Chuck Rakaczky <cjr105@case.edu>
2021-10-06 04:31:27 -0400

    added changing object color by damage level. Applied to bot prefab and sample cubes in beach scene

--- Senior_Project395/Assets/Scenes/Beach Scene/Scripts/Shooting/Damageable.cs
+++ Senior_Project395/Assets/Scenes/Beach Scene/Scripts/Shooting/Damageable.cs
@@ -5,16 +5,25 @@ public class Damageable : MonoBehaviour
 {
 
     //The box's current health point total
-    public int health = 3;
+    public float healthCapacity = 3f;
+    private float currentHealth;
     public int points = 100;
+    public Material[] damageLevelMats;
 
+    public void Start()
+    {
+        currentHealth = healthCapacity;
+    }
     public int doDamage(int damageAmount) //returns points
     {
         //subtract damage amount when Damage function is called
-        health -= damageAmount;
+        currentHealth -= (float)damageAmount;
+        int currentMatIdx = (int)Mathf.Ceil((currentHealth / healthCapacity) * (float)(damageLevelMats.Length - 1));
+        Debug.Log("current Mat: " + currentMatIdx);
+        gameObject.GetComponent<Renderer>().material = damageLevelMats[currentMatIdx];
 
         //Check if health has fallen below zero
-        if (health <= 0)
+        if (currentHealth <= 0f)
         {
             //if health has fallen below zero, deactivate it 
             gameObject.SetActive(false);
